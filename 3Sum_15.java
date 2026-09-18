2db1cd26eb4ba90b3cdb4f2a91cdcea2dbd3f5a3
akarshi Chaudhary <akarshichaudhary25@gmail.com>
2026-09-18 18:51:09 +0530

    Add 3Sum solution with two-pointer and brute force methods
    Implement two-pointer approach for 3Sum problem and add brute force solution.

---
+++ 3Sum_15.java
@@ -0,0 +1,96 @@
+// is like 2sum with target 
+
+sum == 0  → found triplet → left++, right--
+
+sum < 0   → need a BIGGER sum → left++
+
+sum > 0   → need a SMALLER sum → right--
+
+  --> 3Sum = Sort → Fix one element → Two pointers → Handle duplicates.
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            // Skip duplicate first elements      // importnt 
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    ans.add(Arrays.asList(
+                        nums[i],
+                        nums[left],
+                        nums[right]
+                    ));
+
+                    left++;
+                    right--;
+
+                    // Skip duplicates
+                    while (left < right && nums[left] == nums[left - 1])
+                        left++;
+
+                    while (left < right && nums[right] == nums[right + 1])
+                        right++;
+
+                } 
+                else if (sum < 0) {
+                    left++;
+                } 
+                else {
+                    right--;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
+
+
+
+// brute  O(n^3)
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            for (int j = i + 1; j < nums.length - 1; j++) {
+
+                for (int k = j + 1; k < nums.length; k++) {
+
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+
+                        List<Integer> temp = new ArrayList<>();
+
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[k]);
+
+                        Collections.sort(temp);
+
+                        if (!ans.contains(temp)) {
+                            ans.add(temp);
+                        }
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+}
